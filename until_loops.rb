c0ea2d9a83c8b31ad17aec5a34d6b4bb0233ddcf
safrid bhueyan <137524413+safridbhueyan@users.noreply.github.com>
2023-12-13 21:09:32 +0600

    Update until_loops.rb

--- until_loops.rb
+++ until_loops.rb
@@ -3,3 +3,9 @@ until i==10
    puts i*10
      i+=1
 end
+=begin
+Certainly! This Ruby code sets up a loop using `until` that continues until the variable `i` equals `10`. Inside the loop:
+- It prints the current value of `i` multiplied by `10`.
+- `i` is incremented by `1` in each iteration. 
+This process repeats until `i` reaches the value of `10`, at which point the loop stops.
+=end
